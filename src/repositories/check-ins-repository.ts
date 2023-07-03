e7f0fcce19c4c148c7b548bf469d24d123be5a0e
Walefe <walefedev@gmail.com>
2023-07-03 08:17:30 -0300

    feat: implements prisma repository to gyms, users and check in repositories

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,7 +2,7 @@ import { CheckIn, Prisma } from '@prisma/client'
 
 export interface CheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
-  save(checkInd: CheckIn): Promise<CheckIn>
+  save(checkIn: CheckIn): Promise<CheckIn>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
   findById(id: string): Promise<CheckIn | null>
   countByUserId(userId: string): Promise<number>

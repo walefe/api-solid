c00415da00430aa5902db4614082920906f1a02b
Walefe <walefedev@gmail.com>
2023-07-03 09:29:59 -0300

    feat: implements jwt token and verify middleware

---
+++ src/http/controllers/profile.ts
@@ -0,0 +1,16 @@
+import { makeGetUserProfileteUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  const getUserProfile = makeGetUserProfileteUseCase()
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
+}

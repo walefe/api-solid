39658efd4752dcd81d9e6cdde93ed45efcf3f308
Walefe <walefedev@gmail.com>
2023-07-03 16:26:02 -0300

    chore: fix write use case name

--- src/use-cases/get-user-metrics.spec.ts
+++ src/use-cases/get-user-metrics.spec.ts
@@ -1,14 +1,14 @@
 import { it, describe, expect, beforeEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-in-repository'
-import { GetUserMetricsUSeCase } from './get-user-metrics'
+import { GetUserMetricsUseCase } from './get-user-metrics'
 
 let checkInsRepository: InMemoryCheckInsRepository
-let sut: GetUserMetricsUSeCase
+let sut: GetUserMetricsUseCase
 
 describe('Get User Metrics Use Case', () => {
   beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository()
-    sut = new GetUserMetricsUSeCase(checkInsRepository)
+    sut = new GetUserMetricsUseCase(checkInsRepository)
   })
 
   it('should be able to get check-ins count from metrics', async () => {

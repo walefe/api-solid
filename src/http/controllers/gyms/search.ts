116aaf97e325d75d3700de76a0a31639379cb489
Walefe <walefedev@gmail.com>
2023-07-04 20:29:34 -0300

    chore: created controller for gyms

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,21 @@
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+  const { query, page } = searchGymQuerySchema.parse(request.body)
+
+  const makeSearchGymUseCase = makeSearchGymsUseCase()
+  const { gyms } = await makeSearchGymUseCase.execute({
+    query,
+    page,
+  })
+
+  return reply.status(201).send({
+    gyms,
+  })
+}
